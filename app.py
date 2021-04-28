f2206af171329e1f432da6ac8261eefbbed37106
MapleCheng <maplecheng1313@gmail.com>
2021-04-28 19:50:55 +0800

    init: simple HTTP server #20210428001

---
+++ app.py
@@ -0,0 +1,47 @@
+import socket
+
+bind_ip = '0.0.0.0'
+bind_port = 80
+
+def listenTCP():
+  try:
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server_socket.bind(('', bind_port))
+    tcp_server_socket.listen(128)
+
+    return tcp_server_socket
+  except:
+    return 0
+
+def listenRequired(server):
+  client, client_addr = server.accept()
+
+  print('Connected by ', client_addr)
+
+
+  request = client.recv(1024)
+
+  respond = 'GET HTTP/1.1\r\n'
+  respond += '\r\n'
+  respond += 'Hello Python'
+
+  client.send(respond.encode('utf=8'))
+
+  client.close()
+  client,addr = server.accept()
+
+
+def main():
+  server = listenTCP()
+
+  print("server runing in ", bind_port)
+
+
+  while server != 0:
+    listenRequired(server)
+  else:
+    print('Server close')
+
+
+if __name__ == "__main__":
+  main()
